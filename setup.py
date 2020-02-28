ec02401c5d713a26ccbdbe3550a6ec95108b3178
erik.larsen@rainfall.no <eriklarsen.post@gmail.com>
2020-02-28 16:38:28 +0100

    Bump setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="domeneshop-bots",
-    version="0.0.2",
+    version="0.0.1",
     author="Erik Larsen",
     author_email="eriklarsen.post@gmail.com",
-    description="Bot for domeneshop.no to keep your dns records updated with you ip for self hosting sites and services",
+    description="Bot for domeneshop.no to keep your dns records updated with correct ip for your self hosting sites and services",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/larsensolutions/domeneshop.no-bots",
+    url="https://github.com/larsensolutions/domeneshop-bots",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
